ffdcf00bc071fe9204a9d19b016bf54a8cb75d84
Selvio Perez <selvio89@hotmail.com>
2022-02-27 18:06:34 -0500

    Setting cypress up

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     "jest/globals": true,
   },
-  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  extends: ["plugin:react/recommended", "airbnb", "prettier", "plugin:cypress/recommended"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
